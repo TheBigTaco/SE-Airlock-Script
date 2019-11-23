ad18faac18995b09a1130970e48eba139841f63e
TheBigTaco <AdamTitus76@gmail.com>
2019-11-23 00:05:13 -0800

    Initial Commit

---
+++ Testing/Airlock.cs
@@ -0,0 +1,189 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRage;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program
+    {
+        public class Airlock
+        {
+            // Arguments
+            public string DoorSide;
+            public string AirlockName;
+            // List of doors on grid for this script
+            public List<IMyDoor> Doors = new List<IMyDoor>();
+            // List of AirVents on grid for this script
+            public List<IMyAirVent> AirVents = new List<IMyAirVent>();
+            // List of AirVents on grid for this script
+            public List<IMyLightingBlock> Lights = new List<IMyLightingBlock>();
+            // List of SoundBlocks on grid for this script
+            public List<IMySoundBlock> SoundBlocks = new List<IMySoundBlock>();
+            // Current Airlock Doors and AirVents
+            public List<IMyDoor> OpenDoors = new List<IMyDoor>();
+            public List<IMyDoor> ClosedDoors = new List<IMyDoor>();
+            public List<IMyLightingBlock> AirlockLights = new List<IMyLightingBlock>();
+            public List<IMyAirVent> ThisAirlockAirVents = new List<IMyAirVent>();
+            public List<IMySoundBlock> AirlockSoundBlocks = new List<IMySoundBlock>();
+            // Which step it is currently on
+            public int Step = 1;
+            // Count of current check
+            public int Count = 0;
+            // Closed and Open and Active colors
+            Color ready = new Color(0, 255, 0);
+            Color active = new Color(255, 0, 0);
+
+            // Flip state of airlock
+            public void AirlockSwitch()
+            {
+                if (Step == 1)
+                {
+                    foreach (IMyDoor door in OpenDoors)
+                    {
+                        door.Enabled = true;
+                        door.CloseDoor();
+                    }
+                    foreach(IMyLightingBlock light in AirlockLights)
+                    {
+                        light.Color = active;
+                        light.Intensity = 10;
+                        light.Radius = 2;
+                        light.Falloff = 0;
+                        light.BlinkLength = 50;
+                        light.BlinkIntervalSeconds = 1;
+                    }
+                    foreach(IMySoundBlock sb in AirlockSoundBlocks)
+                    {
+                        sb.LoopPeriod = 120;
+                        sb.Play();
+                    }
+                    Step++;
+                }
+                if (Step == 2)
+                {
+                    foreach (IMyDoor door in OpenDoors)
+                    {
+                        if (door.OpenRatio == 0)
+                        {
+                            door.Enabled = false;
+                            Count++;
+                        }
+                    }
+                    if (Count == OpenDoors.Count())
+                    {
+                        Step++;
+                        Count = 0;
+                        return;
+                    }
+                    else
+                    {
+                        Count = 0;
+                    }
+                }
+                if (Step == 3)
+                {
+                    foreach (IMyAirVent vent in ThisAirlockAirVents)
+                    {
+                        if (vent.CanPressurize)
+                        {
+                            if (DoorSide == "Inside")
+                            {
+                                vent.Depressurize = true;
+                            }
+                            else if (DoorSide == "Outside")
+                            {
+                                vent.Depressurize = false;
+                            }
+                        }
+                    }
+                    Step++;
+                }
+                if (Step == 4)
+                {
+                    foreach (IMyAirVent vent in ThisAirlockAirVents)
+                    {
+                        if (DoorSide == "Inside")
+                        {
+                            if (vent.GetOxygenLevel() == 0)
+                            {
+                                Count++;
+                            }
+                        }
+                        else if (DoorSide == "Outside")
+                        {
+                            if (vent.GetOxygenLevel() == 1)
+                            {
+                                Count++;
+                            }
+                        }
+                    }
+                    if (Count == ThisAirlockAirVents.Count())
+                    {
+                        Step++;
+                        Count = 0;
+                        return;
+                    }
+                    else
+                    {
+                        Count = 0;
+                    }
+                }
+                if (Step == 5)
+                {
+                    foreach (IMyDoor door in ClosedDoors)
+                    {
+                        door.Enabled = true;
+                        door.OpenDoor();
+                    }
+                    Step++;
+                }
+                if (Step == 6)
+                {
+                    foreach (IMyDoor door in ClosedDoors)
+                    {
+                        if (door.OpenRatio == 1)
+                        {
+                            door.Enabled = false;
+                            Count++;
+                        }
+                    }
+                    if (Count == ClosedDoors.Count())
+                    {
+                        Step = 0;
+                        Count = 0;
+                        foreach (IMyLightingBlock light in AirlockLights)
+                        {
+                            light.Color = ready;
+                            light.BlinkLength = 0;
+                            light.BlinkIntervalSeconds = 0;
+                        }
+                        foreach (IMySoundBlock sb in AirlockSoundBlocks)
+                        {
+                            sb.Stop();
+                        }
+                        return;
+                    }
+                    else
+                    {
+                        Count = 0;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Testing/Program.cs
@@ -0,0 +1,150 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRage;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program : MyGridProgram
+    {
+        public Program()
+        {
+            Runtime.UpdateFrequency = UpdateFrequency.Update10;
+        }
+
+        // Argument Parser
+        MyCommandLine CommandLine = new MyCommandLine();
+
+        // List of all airlocks currently opening
+        List<Airlock> Airlocks = new List<Airlock>();
+        List<Airlock> AirlockClearList = new List<Airlock>();
+        // INI Object
+        MyIni ini = new MyIni();
+        public void Main(string argument, UpdateType updateType)
+        {
+            if ((updateType & UpdateType.Trigger) != 0)
+            {
+                Airlock airlock = new Airlock();
+                // Parse Arguments
+                if (CommandLine.TryParse(argument))
+                {
+                    airlock.DoorSide = CommandLine.Argument(0);
+                    airlock.AirlockName = CommandLine.Argument(1);
+                }
+
+                // Get a list of all doors for this script
+                GridTerminalSystem.GetBlocksOfType<IMyDoor>(airlock.Doors, door => MyIni.HasSection(door.CustomData, "AirlockScript"));
+                // Get a list of all airvents for this script
+                GridTerminalSystem.GetBlocksOfType<IMyAirVent>(airlock.AirVents, vent => MyIni.HasSection(vent.CustomData, "AirlockScript"));
+                // Get a list of all lights for this script
+                GridTerminalSystem.GetBlocksOfType<IMyLightingBlock>(airlock.Lights, vent => MyIni.HasSection(vent.CustomData, "AirlockScript"));
+                // Get a list of all lights for this script
+                GridTerminalSystem.GetBlocksOfType<IMySoundBlock>(airlock.SoundBlocks, vent => MyIni.HasSection(vent.CustomData, "AirlockScript"));
+
+                // Go through each door in the list until the custom data matches with the inputed arguments
+                foreach (IMyDoor door in airlock.Doors)
+                {
+                    MyIniParseResult result;
+                    if (!ini.TryParse(door.CustomData, out result))
+                    {
+                        throw new Exception(result.ToString());
+                    }
+
+                    if(ini.Get("AirlockScript", "AirlockName").ToString() == airlock.AirlockName)
+                    {
+                        // If the airlock was triggered by the sensor, determine which door needs to close
+                        // Door side passed in should be the door currently open.
+                        if (airlock.DoorSide == "Sensor")
+                        {
+                            if (door.OpenRatio == 1)
+                            {
+                                airlock.DoorSide = ini.Get("AirlockScript", "DoorSide").ToString();
+                            }
+                        }
+                        if (ini.Get("AirlockScript", "DoorSide").ToString() == airlock.DoorSide)
+                        {
+                            airlock.OpenDoors.Add(door);
+                        }
+                        else
+                        {
+                            airlock.ClosedDoors.Add(door);
+                        }
+                    }
+                }
+
+                foreach (IMyAirVent vent in airlock.AirVents)
+                {
+                    MyIniParseResult result;
+                    if (!ini.TryParse(vent.CustomData, out result))
+                    {
+                        throw new Exception(result.ToString());
+                    }
+
+                    if (ini.Get("AirlockScript", "AirlockName").ToString() == airlock.AirlockName)
+                    {
+                        airlock.ThisAirlockAirVents.Add(vent);
+                    }
+                }
+
+                foreach (IMyLightingBlock light in airlock.Lights)
+                {
+                    MyIniParseResult result;
+                    if (!ini.TryParse(light.CustomData, out result))
+                    {
+                        throw new Exception(result.ToString());
+                    }
+
+                    if (ini.Get("AirlockScript", "AirlockName").ToString() == airlock.AirlockName)
+                    {
+                        airlock.AirlockLights.Add(light);
+                    }
+                }
+
+                foreach (IMySoundBlock sb in airlock.SoundBlocks)
+                {
+                    MyIniParseResult result;
+                    if (!ini.TryParse(sb.CustomData, out result))
+                    {
+                        throw new Exception(result.ToString());
+                    }
+
+                    if (ini.Get("AirlockScript", "AirlockName").ToString() == airlock.AirlockName)
+                    {
+                        airlock.AirlockSoundBlocks.Add(sb);
+                    }
+                }
+                Airlocks.Add(airlock);
+            }
+            if ((updateType & UpdateType.Update10) != 0)
+            {
+                foreach(Airlock airlock in Airlocks)
+                {
+                    airlock.AirlockSwitch();
+                    if (airlock.Step == 0)
+                    {
+                        AirlockClearList.Add(airlock);
+                    }
+                }
+                foreach (Airlock airlock in AirlockClearList)
+                {
+                        Airlocks.Remove(airlock);
+                }
+                AirlockClearList.Clear();
+            }
+        }
+    }
+}
